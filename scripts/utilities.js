ed782aff792fd3bb92b11e0d21fa343e61d5c796
LuyandaGitHub <49154286+LuyandaGitHub@users.noreply.github.com>
2020-10-25 18:04:17 +0200

    about page complete

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,19 +1,22 @@
+// // // // // // // // // // // // // // // // // // // // // //
 const menuIcon = document.querySelector('.menu-icon-container');
 const menuContainer = document.querySelector('.menu-container');
 
 
+
+
+
+
+// // // // // // // // // // // // // // // // // // // // // //
 menuIcon.addEventListener('click', toggleMenu);
 
 
+
+
+
+
+// // // // // // // // // // // // // // // // // // // // // //
 function toggleMenu() {
      menuIcon.classList.toggle('active');
      menuContainer.classList.toggle('active');
 }
-
-// var vid = document.getElementById("myVideo");
-
-// function slowPlaySpeed() { 
-//     vid.playbackRate = 0.5;
-// }
-
-// slowPlaySpeed();
