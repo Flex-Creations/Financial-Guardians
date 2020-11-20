141b9842bde856161d1f9691fef68bec3fd73daf
LuyandaGitHub <49154286+LuyandaGitHub@users.noreply.github.com>
2020-11-20 11:05:53 +0200

    added favicon

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,4 @@
 // // // // // // // // // // // // // // // // // // // // // //
-// const workNumber = document.querySelector('.work-number');
-// const staffNumber = document.querySelector('.staff-number');
-// const clientsNumber = document.querySelector('.clients-number');
-// const removalsNumber = document.querySelector('.removals-number');
 
 
 
@@ -10,9 +6,6 @@
 
 
 // // // // // // // // // // // // // // // // // // // // // //
-document.addEventListener("DOMContentLoaded", () => {
-     // setTimeout(count, 1000);
-});
 
 
 
@@ -20,29 +13,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 // // // // // // // // // // // // // // // // // // // // // //
-function counter(element, start, end, duration) {
-     let obj = element,
-     current = start,
-     range = end - start,
-     increment = end > start ? 1 : -1,
-     step = Math.abs(Math.floor(duration/range)),
-
-     timer = setInterval(() => {
-          current += increment;
-          obj.textContent = current;
-          
-          if (current == end) {
-               clearInterval(timer);
-          }
-
-     }, step);
-}
-
-// function count() {
-//      counter(workNumber, 0, 18, 3000);
-//      counter(staffNumber, 0, 9, 2500);
-//      counter(clientsNumber, 0, 35, 3000);
-//      counter(removalsNumber, 0, 33, 3000);
-// }
-
-
